c86782aa3cd7ae47d4154f6dde043e999728c45c
TFQ_Moon <no1msh1217@gmail.com>
2022-02-27 14:02:08 +0900

    방 데이터 표현용 클래스 생성  - 생성자를 커스텀

---
+++ app/src/main/java/com/example/jikbangcopy_220227/datas/RoomData.kt
@@ -0,0 +1,12 @@
+package com.example.jikbangcopy_220227.datas
+
+// 하나의 방 -> 가격, 행정구역, 층수, 설명문구 멤버변수로.
+
+// RoomData 생성자 생성
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String,
+) {
+}
